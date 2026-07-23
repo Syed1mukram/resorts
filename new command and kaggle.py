f9a75e404e5ad388068832bc0f7a1b4800f4b21c
Syed1mukram <syedmukramemail@gmail.com>
2026-07-23 15:30:36 +0530

    Restore best version

---
+++ new command and kaggle.py
@@ -0,0 +1,10 @@
+git add .
+git commit -m "Fix timeline using actual audio duration"
+git push
+
+%cd /kaggle/working
+!rm -rf resorts
+!git clone https://github.com/Syed1mukram/resorts.git
+%cd resorts
+!pip install -q -r requirements.txt
+!python main.py
