0a1ab2d391845d63e524d1ed03bd76f06d55c5ad
Syed1mukram <syedmukramemail@gmail.com>
2026-07-24 05:38:23 +0530

    New hotel

--- src/image_builder.py
+++ src/image_builder.py
@@ -37,8 +37,8 @@ class ImageBuilder:
             VIDEO_HEIGHT / h
         )
 
-        nw = int(np.ceil(w * scale))
-        nh = int(np.ceil(h * scale))
+        nw = int(np.ceil(w * scale * 1.05))
+        nh = int(np.ceil(h * scale * 1.05))
 
         image = cv2.resize(
             image,
